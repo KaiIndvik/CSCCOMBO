3f64663686cb49df8c2655fdd3bc290059c22d83
Indvik <S02366692@ccc.ccofc.edu>
2024-03-19 11:40:40 -0600

    CSC1060

--- GeometricShapes/src/GeometricShapes.java
+++ GeometricShapes/src/GeometricShapes.java
@@ -3,7 +3,7 @@ public class GeometricShapes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Write methods that will calculate area, and perimiter based on what type of shape it is
 	}
 
 }
